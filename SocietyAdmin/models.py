af576af2fd358810cf2bf22a4578480a605e6a09
Shaheer Ramzan <38084480+Shaheerramzan@users.noreply.github.com>
2019-12-21 16:35:12 +0500

    Working on models 2.0

--- SocietyAdmin/models.py
+++ SocietyAdmin/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from Person.models import Person
+from Society.models import Society
 # Create your models here.
+
+
+class SocietyAdmin(models.Model):
+    person_id = models.OneToOneField(Person, on_delete=models.CASCADE)
+    society = models.ForeignKey(Society, on_delete=models.CASCADE)
--- SuperAdmin/models.py
+++ SuperAdmin/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from Person.models import Person
 # Create your models here.
+
+
+class SuperAdmin(models.Model):
+    person_id = models.OneToOneField(Person, on_delete=models.CASCADE)
