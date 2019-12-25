0fd9774832e058b60822fdabb0a3d912356f0b7c
Irtaza Nadeem <irtaza.nadeem542@gmail.com>
2019-12-25 20:05:39 +0500

    Donors urls and views likh deye

--- Donor/urls.py
+++ Donor/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Donor import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('donors/', views.DonorList.as_view(), name='donor-list'),
+    path('donors/<int:pk>', views.DonorDetail.as_view(), name='donor-detail')
 ]
--- Donor/views.py
+++ Donor/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from Donor.models import Donor
+from Donor.serializer import serializers
 # Create your views here.
+
+from rest_framework import generics
+
+
+class DonorList(generics.ListCreateAPIView):
+    queryset = Donor.objects.all()
+    serializer_class = serializers
+
+
+class DonorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Donor.objects.all()
+    serializer_class = serializers
