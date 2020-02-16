06b8d73d11d5e40f52014518e78a578da4797fcc
Shaheer Ramzan <38084480+Shaheerramzan@users.noreply.github.com>
2020-02-16 16:07:22 +0500

    person model update

--- Person/models.py
+++ Person/models.py
@@ -21,7 +21,7 @@ class Person(models.Model):
     longitude = models.FloatField(null=True)
     latitude = models.FloatField(null=True)
     profile_completed = models.BooleanField(default=False)
-    phone = models.OneToOneField(Phone, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
 
 
 class Friend(models.Model):
