ea0b28a834cde6c726b97c42485166382d78fcc5
Shaheer Ramzan <38084480+Shaheerramzan@users.noreply.github.com>
2020-02-16 12:09:43 +0500

    models modified

--- History/models.py
+++ History/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from Person.models import Person
+
+
 # Create your models here.
 
 
@@ -7,4 +9,4 @@ class History(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     time = models.TimeField()
     completion_status = models.BooleanField()
-    history_type = models.BooleanField(default=True)
+    history_type = models.BooleanField(default=True)  # True means BloodHistory False means ConveyanceHistory
